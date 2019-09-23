ee9c4f2fb885e787d8f6adba07405944bb88580d
Arun Kumar P <reacharun@email.com>
2019-09-23 21:08:28 +0530

    Part 3 completed - Standard Navigation Bar - Login Form layout

--- public_html/index.php
+++ public_html/index.php
@@ -14,14 +14,41 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <!-- My own javascript file -->
     <script type="text/javascript" src="./js/main.js"></script>
 
     <title> Inventory Management System</title>
 </head>
 <body>
-    <div class="jumbotron">
-        <h1>Hello World</h1>
-    </div>
+    <!-- Navigation Bar -->
+    <?php include_once("./templates/header.php"); ?>
+    <br/><br/>
+
+    <div class="container">
+        <!-- Card -->
+        <div class="card mx-auto" style="width: 20rem;">
+        <img class="card-img-top mx-auto" style="width:60%;" src="./images/login.png" alt="Login Icon">
+            <div class="card-body">
+                <!-- Form -->
+                <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+
+                <button type="submit" class="btn btn-primary"><i class="fa fa-lock"></i> &nbsp; Login</button>
+                <span><a href="#">Register</a></span>
+                </form>
+            </div>
+            <div class="card-footer"><a href="#">Forgot Password?</a></div>
+        </div>
+    </div> 
 </body>
 </html>
